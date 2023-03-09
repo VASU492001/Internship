90ce46eb1bde83e649d4c5106e2d858b4141886e
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-09 11:45:46 +0530

    DOM

---
+++ DOM/DOM.js
@@ -0,0 +1,36 @@
+$("document").ready(function () {
+
+    // $(".div1").click(function () {
+    //     var x = $(".p1").html();
+    //     $("#span1").text(x)
+    // })
+
+    // $(".div1").click(function () {
+    //     alert($(".p1").text());
+    // })
+
+    // $("#btn1").click(function () {
+    //     $(".p1").attr("id", "p2id")
+    // })
+
+    // $(".p1").click(function () {
+    //     alert($(".p1").attr("id"))
+    // })
+
+    // $("#btn1").click(function(){
+    //     $(".div1").css("background-color", "green")
+    // })
+    // $(".div1").click(function () {
+    //     alert($(".div1").css("background-color"));
+    // })
+
+    // $("#btn1").click(function () {
+    //     $(".div1").append("<p>This is paragraph 2</p>")
+    // })
+
+    $("#btn1").click(function () {
+        $(".div1").append('<img src="Image1.png" height="100px" width="100px">')
+
+
+    })
+})
