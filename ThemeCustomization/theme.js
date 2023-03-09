fc3707159597664bb1026ef9b3027c4361c240a1
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-09 17:09:48 +0530

    Theme

---
+++ ThemeCustomization/theme.js
@@ -0,0 +1,15 @@
+$("document").ready(function () {
+    $("#dialog").dialog({
+        draggable: true,
+        resizable: true,
+        closeOnEscape: true,
+        modal: false,
+        autoOpen: true
+    })
+
+    $("#date").datepicker({
+        showOtherMonths: true,
+        changeMonth: true,
+        changeYear: true
+    })
+})
