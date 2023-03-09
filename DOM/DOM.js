41398f32cc8236690b457d3080ea32e081114139
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-09 12:23:09 +0530

    DOM

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -33,4 +33,24 @@ $("document").ready(function () {
 
 
     })
-})
+
+    $("#btn2").click(function () {
+        $(".div1").prepend("<p>This is paragraph 2 using prepend</p>")
+    })
+
+    $("#btn3").click(function () {
+        $(".div1").before("<p>This is paragraph 2 using before</p>")
+    })
+
+    $("#btn4").click(function () {
+        $(".div1").after("<p>This is paragraph 2 using after</p>")
+    })
+
+    $("#btn5").click(function () {
+        $(".div1").remove()
+    })
+
+    $("#btn6").click(function () {
+        $(".div1").empty()
+    })
+});
