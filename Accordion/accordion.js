0adb921e2816e07278fc77672d821ddd5cc51ff5
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-09 13:27:17 +0530

    Accordion

---
+++ Accordion/accordion.js
@@ -0,0 +1,9 @@
+$("document").ready(function () {
+    $("#div1").accordion({
+        collapsible: true,
+        icons: { header: "üi - icon - caret - 1 - s", activeHeader: "ui-icon-caret-1-n" },
+        animate: 2000,
+        event: "mouseover"
+    });
+
+})
